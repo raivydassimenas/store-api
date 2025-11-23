05071d23ca63bda124e70ec2c72c652e113e960a
Raivydas Šimėnas <raivydas.simenas@icloud.com>
2025-11-23 15:17:25 +0200

    Initial commit

---
+++ src/main/java/me/raivydassimenas/store_backend/StoreBackendApplication.java
@@ -0,0 +1,13 @@
+package me.raivydassimenas.store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreBackendApplication.class, args);
+	}
+
+}
